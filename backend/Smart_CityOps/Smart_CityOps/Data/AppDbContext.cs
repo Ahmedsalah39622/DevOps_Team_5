8dfc9ccaa7597201fb31223c7f79648bb27c5452
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2025-11-28 21:32:34 +0200

    Added SmartCityDbContext and GetCurrentWeather, GetByType, GetSummary

--- backend/Smart_CityOps/Smart_CityOps/Data/AppDbContext.cs
+++ backend/Smart_CityOps/Smart_CityOps/Data/AppDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Smart_CityOps.Entities;
 
 namespace Smart_CityOps.Data
 {
@@ -9,5 +10,6 @@ namespace Smart_CityOps.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+        public DbSet<SensorData> SensorData { get; set; }
     }
 }
